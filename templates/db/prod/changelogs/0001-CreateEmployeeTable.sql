cb84fc5140efe64c3daf92a4a6894dc86dd8455e
Reljod Oreta <oretareljod@gmail.com>
2024-01-17 23:35:51 +0800

    Update employee table to default current timestamp

--- templates/db/prod/changelogs/0001-CreateEmployeeTable.sql
+++ templates/db/prod/changelogs/0001-CreateEmployeeTable.sql
@@ -9,10 +9,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS employees (
     id SERIAL PRIMARY KEY,
     employee_id uuid DEFAULT uuid_generate_v4(),
-    employee_number SERIAL UNIQUE NOT NULL,
     company_id uuid NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status VARCHAR(50) NOT NULL
 );
 
